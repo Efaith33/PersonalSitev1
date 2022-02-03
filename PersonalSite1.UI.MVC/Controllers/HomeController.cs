60411e034cd1a9fcbee8ad8665744f257632283d
ERIC Weibley <weibleyeric@yahoo.com>
2022-02-02 23:59:55 -0600

    added lincoln quote & updated resume.pdf

--- PersonalSite1.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite1.UI.MVC/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using PersonalSite1.UI.MVC.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace PersonalSite1.UI.MVC.Controllers
@@ -25,6 +25,43 @@ namespace PersonalSite1.UI.MVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br/>{cvm.Message}";
+
+            MailMessage mm = new MailMessage("admin@eric-faith.net", "weibleyeric@yahoo.com", cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+
+            SmtpClient client = new SmtpClient("mail.eric-faith.net");
+            client.Port = 25; 
+            client.EnableSsl = false;
+
+            client.Credentials = new NetworkCredential("admin@eric-faith.net", "P@ssw0rd");
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're sorry your request could not be completed at this time. Please try again later. Error Message: {ex.Message}<br/>{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+
         }
     }
 }
